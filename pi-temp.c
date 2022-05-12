dcc34d81fe53c4e7de55e731e441ed10b64159e3
Aleksejs Abrosimovs <aleksejs.abrosimovs@gmail.com>
2022-05-12 23:04:38 +0300

    grand rewriting argument are added

--- pi-temp.c
+++ pi-temp.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
+/*
 BUFSIZE is for vcgencmd measure_temp command output
- 
-#define BUFSIZE 32
+it should not be more then 16 chars.
+*/
+#define BUFSIZE 16
 
 /*
 Usage:
@@ -12,7 +16,7 @@ Usage:
 Monitor CPU temperature.
 
 Options:
- -c			run using "vcgencmd measure_temp" command, this is defualt
+ -c			run using "vcgencmd measure_temp" command, this is default
  -f			run reading file /sys/class/thermal/thermal_zone0/temp
  -d	[NUM]	delay in second between measures, this option should be
 			used separetely, default is 3.
@@ -22,33 +26,88 @@ Options:
 Examples:
  pi-temp -f -t -l -d 3 
 */
- 
-int main(int arvc, char **argv) {
-    char *cmd = "/usr/bin/vcgencmd measure_temp";
-    char *cls = "/usr/bin/clear";
+void print_temp_cmd(void);
+
+int main(int argc, char **argv) {
+    short sleepsec = 3;
+    /* run-type, 1 for -c flag, 0 for -f flag */
+    short run_type = 1;
+    /* ts_bool, timestamp, 1 for -t flag, 0 for none */
+    short ts_bool = 0;
+    /* log_bool, log write, 1 for -l flag, 0 for none */
+    short log_bool = 0;
+    short i = 0;
+    
+    /* checking arguments */
+    if (argc > 1) {
+		for (i = 1; i < argc; i++) {
+			if (strcmp(argv[i], "-c") == 0)
+				run_type = 1;
+			else if (strcmp(argv[i], "-f") == 0)
+				run_type = 0;
+			else if (strcmp(argv[i], "-t") == 0)
+				ts_bool = 1;
+ 			else if (strcmp(argv[i], "-l") == 0)
+				log_bool = 1;
+			else if (strcmp(argv[i], "-d") == 0) {
+				i++; 
+				sleepsec = atoi(argv[i]);
+			}
+		}
+	}
+	
+/*
+DEBUG
+printing out arguments
+*/
+	if (run_type)
+		printf("flag -c was used\n");
+	else
+		printf("flag -f was used\n");
+	
+	if (ts_bool)
+		printf("flag -t was used\n");
+	if (log_bool)
+		printf("flag -l was used\n");
+	printf("delay = %d seconds\n", sleepsec);
+	
+	printf("\nprinting arguments:\n");
+	for (i = 1; i < argc; i++)
+		printf("argument %d = %s\n", i, argv[i]);
+/*
+END o DEBUG */
+					
+				
+    
+/*
+clean screen
+    printf("\e[1;1H\e[2J");
+*/
+
+
+    
+    return 0;
+}
+
+void print_temp_cmd(void) {
+	char *cmd = "/usr/bin/vcgencmd measure_temp";
     char buf[BUFSIZE];
     FILE *fp;
-    int sleepsec = 1;
-    long unsigned count = 0;
-    if ((fp = popen(cmd, "r")) == NULL) {
-        printf("Error opening pipe!\n");
-        return -1;
+	
+	/* opening pipe and if everything is OK - printing temp */
+	if ((fp = popen(cmd, "r")) != NULL) 
+        if (fgets(buf, BUFSIZE, fp) != NULL)
+			printf("%s", buf);
+		else {
+			printf("Error getting result from command\n");
+			exit(1);
+		}
+    else {
+		printf("Error opening pipe!\n");
+        exit(127);
     }
-    pclose(fp);
-    while (1 > 0) {
-        printf("\e[1;1H\e[2J");
-        fp = popen(cmd, "r");
-        while (fgets(buf, BUFSIZE, fp) != NULL)
-            printf("%d: %s", count, buf);
-        sleep(sleepsec);
-        count++;
-        pclose(fp);
-    }
-
     if (pclose(fp)) {
-        printf("Command not found or exited with error status\n");
-        return -1;
-    }
-
-    return 0;
+		printf("Error closing pipe\n");
+		exit(1);
+	}
 }
