d72e92d3c52eeadc60ebce498d67518d148a219d
Aleksejs Abrosimovs <aleksejs.abrosimovs@gmail.com>
2022-05-12 18:43:31 +0300

    comment added

--- pi-temp.c
+++ pi-temp.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
 
+BUFSIZE is for vcgencmd measure_temp command output
+ 
 #define BUFSIZE 32
 
+/*
+Usage:
+ pi-temp [options]
+
+Monitor CPU temperature.
+
+Options:
+ -c			run using "vcgencmd measure_temp" command, this is defualt
+ -f			run reading file /sys/class/thermal/thermal_zone0/temp
+ -d	[NUM]	delay in second between measures, this option should be
+			used separetely, default is 3.
+ -t			print timestamp before temperature
+ -l			write results in file, default file ./pi-temp.log
+
+Examples:
+ pi-temp -f -t -l -d 3 
+*/
+ 
 int main(int arvc, char **argv) {
     char *cmd = "/usr/bin/vcgencmd measure_temp";
     char *cls = "/usr/bin/clear";
