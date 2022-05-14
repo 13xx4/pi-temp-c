b30089c76b3ad05f7c00312d28e91ff15cabb509
Aleksejs Abrosimovs <aleksejs.abrosimovs@gmail.com>
2022-05-14 14:29:08 +0300

    added -r and -h flags

--- pi-temp.c
+++ pi-temp.c
@@ -30,13 +30,16 @@ Examples:
 void print_temp_cmd(void);
 void print_temp_file(void);
 void print_timestamp(void);
+void print_help(void);
 
 int main(int argc, char **argv) {
-    short sleepsec = 3;
+    short sleepsec = 2;
     /* run-type, 1 for -c flag, 0 for -f flag */
     short run_type = 1;
     /* ts_bool, timestamp, 1 for -t flag, 0 for none */
     short ts_bool = 0;
+    /* cls_bool, 1 for -r flag */
+    short cls_bool = 0;
     short i = 0;
 
     /* checking arguments */
@@ -48,9 +51,15 @@ int main(int argc, char **argv) {
 				run_type = 0;
 			else if (strcmp(argv[i], "-t") == 0)
 				ts_bool = 1;
+			else if (strcmp(argv[i], "-r") == 0)
+				cls_bool = 1;
 			else if (strcmp(argv[i], "-d") == 0) {
 				i++; 
 				sleepsec = atoi(argv[i]);
+			} 
+			else if ((strcmp(argv[i], "--help")) || (strcmp(argv[i], "-h"))) {
+				print_help();
+				exit(0);
 			}
 		}
 	}
@@ -62,6 +71,8 @@ int main(int argc, char **argv) {
 				print_timestamp();
 			print_temp_cmd();
 			sleep(sleepsec);
+			if (cls_bool)
+			    printf("\e[1;1H\e[2J");
 		}
 	else
 		while (1 > 0) {
@@ -69,6 +80,8 @@ int main(int argc, char **argv) {
 				print_timestamp();
 			print_temp_file();
 			sleep(sleepsec);
+			if (cls_bool)
+			    printf("\e[1;1H\e[2J");
 		}
 		
 /*
@@ -136,4 +149,22 @@ void print_timestamp(void){
 	tm_info = localtime(&now);
 	strftime(timebuf, 21, "%Y.%m.%d %H:%M:%S ", tm_info);
 	printf("%s", timebuf);
+}
+
+void print_help(void) {
+	printf("\n\
+Usage: ./[program name] [options]\n\
+Monitor CPU temperature.\n\
+\n\
+Options:\n\
+  -c\t\trun using \"vcgencmd measure_temp\" command, this is default\n\
+  -f\t\trun reading file /sys/class/thermal/thermal_zone0/temp\n\
+  -d [NUM]\tdelay in second between measures, this option should be\n\
+\t\tused separetely, default is 2.\n\
+  -t\t\tprint timestamp before temperature\n\
+  -r\t\tuse monitor mode instead of history mode.\n\
+  -h, --help\tshow this help\n\
+\n\
+Examples:\n\
+  pi-temp -f -t -d 3\n");
 }
